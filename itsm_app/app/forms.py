abfc6a48242938168da2f3c1ea3fe8a541af4724
Tony Lofreso Jr <tonylofresojr@gmail.com>
2021-03-07 22:45:17 -0500

    added additional fields to server build form. Fields encompass all required parameters to build a new DO droplet.

--- itsm_app/app/forms.py
+++ itsm_app/app/forms.py
@@ -1,8 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField, SelectField
 from wtforms.validators import DataRequired
 
 
 class NewServer(FlaskForm):
     name = StringField("Frendly Name", validators=[DataRequired()])
+    region = RadioField(
+        "Server Region",
+        validators=[DataRequired()],
+        choices=[(1, "nyc1"), (2, "nyc2")],
+        default=1,
+    )
+    size = RadioField(
+        "Server Size",
+        validators=[DataRequired()],
+        choices=[(1, "small"), (2, "medium"), (3, "large")],
+        default=1,
+    )
+    image = SelectField(
+        "Image", validators=[DataRequired()], choices=["Ubuntu", "Fedora"]
+    )
     submit = SubmitField("Build")
--- itsm_app/app/routes.py
+++ itsm_app/app/routes.py
@@ -1,9 +1,21 @@
-from flask import render_template
+from flask import render_template, redirect
 from app import app
 from app.forms import NewServer
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def web_form():
     form = NewServer()
+    if form.validate_on_submit():
+        name = form.name.data
+        region = form.region.data
+        size = form.size.data
+        image = form.image.data
+
+        payload = {"name": name, "region": region, "size": size, "image": image}
+
+        print(payload)
+
+        return redirect("/")
+
     return render_template("index.html", title="Home", form=form)
