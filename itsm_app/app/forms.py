96f9c44c0ec61a1de380f3081361fa6291c8bc19
Tony Lofreso Jr <tonylofresojr@gmail.com>
2021-03-07 21:06:26 -0500

    added forms

--- itsm_app/app/forms.py
+++ itsm_app/app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class NewServer(FlaskForm):
     name = StringField("Frendly Name", validators=[DataRequired()])
+    submit = SubmitField("Build")
--- itsm_app/app/routes.py
+++ itsm_app/app/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from app import app
+from app.forms import NewServer
 
 
 @app.route("/")
 def web_form():
-    return render_template("index.html", title="Home")
+    form = NewServer()
+    return render_template("index.html", title="Home", form=form)
